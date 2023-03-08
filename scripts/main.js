b70bc5ea8ed82fc2a4f49d3fe203a1fc08d0a306
chelseaY119 <ayang106@my.bcit.ca>
2023-03-08 12:34:29 -0800

    create a after_login page, connect with database, sort js files

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,34 @@
 
 
-// for temperature slider
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-var confirmButton = document.getElementById("confirmButton");
+// // for temperature slider
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// var confirmButton = document.getElementById("confirmButton");
 
 
-// output.innerHTML = slider.value;
+// // output.innerHTML = slider.value;
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
-}
+// slider.oninput = function () {
+//   output.innerHTML = this.value;
+// }
 
-confirmButton.addEventListener("click", function () {
-  var value = slider.value;
-  var confirmValue = confirm("Are you sure you want to select " + value + "?");
+// confirmButton.addEventListener("click", function () {
+//   var value = slider.value;
+//   var confirmValue = confirm("Are you sure you want to select " + value + "?");
 
-  if (confirmValue) {
-    if (value > 54) {
-      openPopup2();
-      // window.open("popup1.html", "Popup", "width=400,height=400", "openPopup()");
-    }
+//   if (confirmValue) {
+//     if (value > 54) {
+//       openPopup2();
+//       // window.open("popup1.html", "Popup", "width=400,height=400", "openPopup()");
+//     }
 
-    else {
-      // slider.value = 23; // Reset the slider to the default value if the user cancels
-      // output.innerHTML = slider.value;
-      window.location.href = "../heatindex.html";
-    }
-  }
-});
+//     else {
+//       // slider.value = 23; // Reset the slider to the default value if the user cancels
+//       // output.innerHTML = slider.value;
+//       window.location.href = "../heatindex.html";
+//     }
+//   }
+// });
 
 //for share popup
 let popup1 = document.getElementById("popup1");
@@ -42,37 +42,37 @@ function closePopup1() {
 }
 
 
-// for warning pop
-let popup2 = document.getElementById("popup2");
-
-function openPopup2() {
-  popup2.classList.add("open-popup2");
-
-  // Add a button to the popup that redirects the user
-  var popupButton = document.createElement("button");
-  popupButton.innerText = "OK";
-  popupButton.addEventListener("click", function () {
-    // Redirect the user to a different page
-    window.location.href = "../heatindex.html";
-  });
-  popup2.appendChild(popupButton);
-}
+// // for warning pop
+// let popup2 = document.getElementById("popup2");
 
 // function openPopup2() {
 //   popup2.classList.add("open-popup2");
+
+//   // Add a button to the popup that redirects the user
+//   var popupButton = document.createElement("button");
+//   popupButton.innerText = "OK";
+//   popupButton.addEventListener("click", function () {
+//     // Redirect the user to a different page
+//     window.location.href = "../heatindex.html";
+//   });
+//   popup2.appendChild(popupButton);
 // }
 
-// function closePopup2() {
-//   popup2.classList.remove("open-popup2");
-// }
+// // function openPopup2() {
+// //   popup2.classList.add("open-popup2");
+// // }
 
-// Get a reference to the button element
-var myButton = document.getElementById("othersfeelbtn");
+// // function closePopup2() {
+// //   popup2.classList.remove("open-popup2");
+// // }
 
-// Add an event listener to the button that redirects the user to a new page
-myButton.addEventListener("click", function () {
-  window.location.href = "../heatindex.html";
-});
+// // Get a reference to the button element
+// var myButton = document.getElementById("othersfeelbtn");
+
+// // Add an event listener to the button that redirects the user to a new page
+// myButton.addEventListener("click", function () {
+//   window.location.href = "../heatindex.html";
+// });
 
 
 
