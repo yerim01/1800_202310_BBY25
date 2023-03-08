dab27a06e702f832eb75a45b22c88738f82a1de1
chelseaY119 <ayang106@my.bcit.ca>
2023-03-07 23:54:30 -0800

    add emergency_contact html, css file. move slidebar/popup js to a separate js file.

---
+++ scripts/slidebar_popup.js
@@ -0,0 +1,51 @@
+ 
+
+      // for temperature slider
+      var slider = document.getElementById("myRange");
+      var output = document.getElementById("demo");
+      var confirmButton = document.getElementById("confirmButton");
+
+
+      // output.innerHTML = slider.value;
+
+      slider.oninput = function () {
+        output.innerHTML = this.value;
+      }
+
+      confirmButton.addEventListener("click", function () {
+        var value = slider.value;
+        var confirmValue = confirm("Are you sure you want to select " + value + "?");
+
+        if (confirmValue) {
+          if (value > 54) {
+            openPopup2();
+            // window.open("popup1.html", "Popup", "width=400,height=400", "openPopup()");
+          }
+        } else {
+          slider.value = 23; // Reset the slider to the default value if the user cancels
+          output.innerHTML = slider.value;
+        }
+      });
+
+      //for share popup
+      let popup1 = document.getElementById("popup1");
+
+      function openPopup1() {
+        popup1.classList.add("open-popup1");
+      }
+
+      function closePopup1() {
+        popup1.classList.remove("open-popup1");
+      }
+
+
+      // for warning pop
+      let popup2 = document.getElementById("popup2");
+
+      function openPopup2() {
+        popup2.classList.add("open-popup2");
+      }
+
+      function closePopup2() {
+        popup2.classList.remove("open-popup2");
+      }
