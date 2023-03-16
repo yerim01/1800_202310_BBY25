ab6b714008fea1bee8747cf1e24252f4d04368a2
Yerim Moon <myr9901@gmail.com>
2023-03-16 15:45:03 -0700

    join.js added by Yerim

---
+++ scripts/join.js
@@ -0,0 +1,49 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLPuPEMw5VV-DvYSIQZjyc7X5SKzK3bBY",
+  authDomain: "heatpro-62070.firebaseapp.com",
+  projectId: "heatpro-62070",
+  storageBucket: "heatpro-62070.appspot.com",
+  messagingSenderId: "483967183474",
+  appId: "1:483967183474:web:0adf32d917571d857b30eb",
+  measurementId: "G-2E6H9F7XEV",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+var collectionRef = db.collection("users");
+
+document.getElementById("register").addEventListener("click", (event) => {
+  event.preventDefault();
+  const signUpemail = document.getElementById("signUpEmail").value;
+  const signUppassword = document.getElementById("signUpPassword").value;
+  const displayName = document.getElementById("displayName").value;
+
+  const auth = firebase.auth();
+
+  auth
+    .createUserWithEmailAndPassword(signUpemail, signUppassword)
+    .then((userCredential) => {
+      console.log(userCredential);
+      // Signed in
+      const user = userCredential.user;
+
+      collectionRef
+        .add({
+          name: displayName,
+          email: signUpemail,
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+          window.location.assign("../main.html");
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    })
+    .catch((error) => {
+      console.log("login failed");
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+});
